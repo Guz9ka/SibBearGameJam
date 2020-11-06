ad207af22b2f6a29d756a467b30ad35029c22175
Guz9ka <60352331+Guz9ka@users.noreply.github.com>
2020-11-06 21:12:37 +0300

    Добавил перса

---
+++ Assets/Scripts/Balance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balance : MonoBehaviour
+{
+    public float targetRotation;
+    Rigidbody2D rb;
+    public float force;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.MoveRotation(Mathf.LerpAngle(rb.rotation, targetRotation, force * Time.fixedDeltaTime));
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float horizontalForce;
+    float horizontalInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void Move()
+    {
+        Vector2 force = new Vector2(horizontalForce * horizontalInput, 0);
+        rb.AddForce(force * Time.deltaTime);
+    }
+}
