a766bbd6f57dc4f445ec15607a780521cc39c8cc
Guz9ka <60352331+Guz9ka@users.noreply.github.com>
2020-11-08 12:01:13 +0300

    Добавил скрипт проигрывания звуков

---
+++ Assets/Scripts/SoundsPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsPlayer : MonoBehaviour
+{
+    public static SoundsPlayer singleton { get; private set; }
+
+    private void Start()
+    {
+        singleton = this;
+    }
+
+    public AudioSource switcherSound;
+    public AudioSource openElectricityStandSound;
+    public AudioSource burpSound;
+    public AudioSource breakBulbSound;
+    public AudioSource plugInWireSound;
+    public AudioSource screwInBulbSound;
+
+    public void PlaySoundSwitchSwitcher() 
+    {
+        switcherSound.Play();
+    }
+
+    public void PlaySoundOpenElectricityStand()
+    {
+        openElectricityStandSound.Play();
+    }
+
+    public void PlaySoundBurp()
+    {
+        burpSound.Play();
+    }
+
+    public void PlaySoundBreakBulb()
+    {
+        breakBulbSound.Play();
+    }
+
+    public void PlaySoundBulbScrewIn()
+    {
+        screwInBulbSound.Play();
+    }
+
+    public void PlaySoundPlugInWire()
+    {
+        plugInWireSound.Play();
+    }
+}
